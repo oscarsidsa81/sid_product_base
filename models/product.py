d72801c4cc1c2365defcb825c9ca5b92ed99a542
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-19 13:45:10 +0100

    modifica manifest

--- models/product.py
+++ models/product.py
@@ -6,36 +6,36 @@ class ProductTemplate(models.Model):
 
     sid_pasillo = fields.Many2one(
         "sid.location.option",
-        string="Pasillo (SID)",
+        string="Pasillo",
         domain="[(\"location_type\", \"=\", \"pasillo\"), (\"active\", \"=\", True)]",
     )
     sid_alto = fields.Many2one(
         "sid.location.option",
-        string="Alto (SID)",
+        string="Alto",
         domain="[(\"location_type\", \"=\", \"alto\"), (\"active\", \"=\", True)]",
     )
     sid_lado = fields.Many2one(
         "sid.location.option",
-        string="Lado (SID)",
+        string="Lado",
         domain="[(\"location_type\", \"=\", \"lado\"), (\"active\", \"=\", True)]",
     )
     sid_largo = fields.Many2one(
         "sid.location.option",
-        string="Largo (SID)",
+        string="Largo",
         domain="[(\"location_type\", \"=\", \"largo\"), (\"active\", \"=\", True)]",
     )
 
-    sid_pasillo = fields.Char(string="Pasillo (SID)")
-    sid_alto = fields.Char(string="Alto (SID)")
-    sid_lado = fields.Char(string="Lado (SID)")
-    sid_largo = fields.Char(string="Largo (SID)")
+    sid_pasillo = fields.Char(string="Pasillo")
+    sid_alto = fields.Char(string="Alto")
+    sid_lado = fields.Char(string="Lado")
+    sid_largo = fields.Char(string="Largo")
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     sid_pasillo = fields.Many2one(
         "sid.location.option",
-        string="Pasillo (SID)",
+        string="Pasillo",
         domain="[(\"location_type\", \"=\", \"pasillo\"), (\"active\", \"=\", True)]",
     )
-    sid_pasillo = fields.Char(string="Pasillo (SID)")
+    sid_pasillo = fields.Char(string="Pasillo")
