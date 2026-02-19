f42e2e3f3a58ba197838783c214e4405370add5e
oscarsidsa81 <78971905+oscarsidsa81@users.noreply.github.com>
2026-02-19 12:48:40 +0100

    Remove unavailable dependency from sid_product_base

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
   "name": "SID Product Base",
   "version": "15.0.1.0.0",
-  "depends": ["product", "oct_product_extra_fields"],
+  "depends": ["product"],
   "data": [],
   "installable": True,
   "application": False
