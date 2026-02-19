0ea9661df8dcd1ec50f62fb8aa78900e1cfc78d5
oscarsidsa81 <78971905+oscarsidsa81@users.noreply.github.com>
2026-02-19 12:36:53 +0100

    Fix SID product fields to use Char types

--- models/product.py
+++ models/product.py
@@ -3,13 +3,13 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    sid_pasillo = fields.Selection(string="Pasillo (SID)")
-    sid_alto = fields.Selection(string="Alto (SID)")
-    sid_lado = fields.Selection(string="Lado (SID)")
-    sid_largo = fields.Selection(string="Largo (SID)")
+    sid_pasillo = fields.Char(string="Pasillo (SID)")
+    sid_alto = fields.Char(string="Alto (SID)")
+    sid_lado = fields.Char(string="Lado (SID)")
+    sid_largo = fields.Char(string="Largo (SID)")
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    sid_pasillo = fields.Selection(string="Pasillo (SID)")
+    sid_pasillo = fields.Char(string="Pasillo (SID)")
     sid_forecast_madrid = fields.Float(string="Forecast Madrid (SID)")
