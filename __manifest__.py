6f521d0a4ea9fd24016302ee53b57010751e854c
oscarsidsa81 <78971905+oscarsidsa81@users.noreply.github.com>
2026-02-19 13:19:09 +0100

    Add editable SID location table in technical settings

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,11 @@
   "name": "SID Product Base",
   "version": "15.0.1.0.0",
   "depends": ["product"],
-  "data": [],
+  "data": [
+    "security/ir.model.access.csv",
+    "views/sid_location_views.xml",
+    "data/sid_location_data.xml"
+  ],
   "installable": True,
   "application": False
 }
--- models/product.py
+++ models/product.py
@@ -1,15 +1,37 @@
 from odoo import fields, models
 
+
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    sid_pasillo = fields.Char(string="Pasillo (SID)")
-    sid_alto = fields.Char(string="Alto (SID)")
-    sid_lado = fields.Char(string="Lado (SID)")
-    sid_largo = fields.Char(string="Largo (SID)")
+    sid_pasillo = fields.Many2one(
+        "sid.location.option",
+        string="Pasillo (SID)",
+        domain="[(\"location_type\", \"=\", \"pasillo\"), (\"active\", \"=\", True)]",
+    )
+    sid_alto = fields.Many2one(
+        "sid.location.option",
+        string="Alto (SID)",
+        domain="[(\"location_type\", \"=\", \"alto\"), (\"active\", \"=\", True)]",
+    )
+    sid_lado = fields.Many2one(
+        "sid.location.option",
+        string="Lado (SID)",
+        domain="[(\"location_type\", \"=\", \"lado\"), (\"active\", \"=\", True)]",
+    )
+    sid_largo = fields.Many2one(
+        "sid.location.option",
+        string="Largo (SID)",
+        domain="[(\"location_type\", \"=\", \"largo\"), (\"active\", \"=\", True)]",
+    )
+
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    sid_pasillo = fields.Char(string="Pasillo (SID)")
+    sid_pasillo = fields.Many2one(
+        "sid.location.option",
+        string="Pasillo (SID)",
+        domain="[(\"location_type\", \"=\", \"pasillo\"), (\"active\", \"=\", True)]",
+    )
     sid_forecast_madrid = fields.Float(string="Forecast Madrid (SID)")
